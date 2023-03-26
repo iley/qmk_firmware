109f71e75abfa2687d0d7eebb02d40d2af54f88a
Ilya Strukov <iley@iley.ru>
2023-03-26 16:40:15 +0100

    Update to newer upstream version

--- keyboards/ergodox_ez/keymaps/ergomac/keymap.c
+++ keyboards/ergodox_ez/keymaps/ergomac/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC  ,KC_A       ,KC_S    ,KC_D    ,KC_F    ,KC_G ,
         KC_LSFT ,CTL_T(KC_Z),KC_X    ,KC_C    ,KC_V    ,KC_B ,LGUI(KC_SPC),
         _______ ,KC_GRV     ,KC_BSLS ,KC_LEFT ,KC_RGHT ,
-                                                                      KC_LGUI, KC_LCTRL,
+                                                                      KC_LGUI, KC_LCTL,
                                                                                KC_LALT,
                                                               KC_BSPC,KC_DEL,  MO(1),
         // right hand
